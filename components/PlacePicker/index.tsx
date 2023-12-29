e1c6b5c61528054dcbd63965654b242b54ab33d7
youngduck <dudejr5839@naver.com>
2023-12-29 13:12:40 +0900

    fix: home DD-14 wireframe refactor

---
+++ components/PlacePicker/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Input from "@/components/Input";
+
+const PlacePicker = () => {
+  return (
+    <>
+      <Input
+        className="form-layout card-layout-rounded-full"
+        content="이름"
+        placeholder="이름"
+      />
+      <Input
+        className="form-layout card-layout-rounded-full"
+        content="장소"
+        placeholder="장소"
+      />
+    </>
+  );
+};
+
+export default PlacePicker;
