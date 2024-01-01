f1bf6bdc31d492daa02cbd0bbe8073f647be6ddf
youngduck <dudejr5839@naver.com>
2024-01-02 08:59:09 +0900

    feat: DD-14 schedule wire frame

--- components/Picker/TimePicker.tsx
+++ components/Picker/TimePicker.tsx
@@ -1,19 +1,19 @@
 "use client";
 import React from "react";
-import Svg_down from "@/public/svgs/down.svg";
+import SVG_down from "@/public/svgs/down.svg";
 
 const TimePicker = () => {
   return (
     <div className="form-layout card-layout-rounded-full centered-content justify-evenly">
       <span className="w-[40px]">시간</span>
       <div className="centered-content w-[270px]">
-        <Svg_down />
+        <SVG_down />
         <span>09:00 -</span>
 
-        <Svg_down />
+        <SVG_down />
         <span className="mr-[16px]">22:00</span>
 
-        <Svg_down />
+        <SVG_down />
         <span>2시간</span>
       </div>
     </div>
--- containers/schedule/ScheduleSummary/index.tsx
+++ containers/schedule/ScheduleSummary/index.tsx
@@ -1,7 +1,55 @@
+"use client";
+
 import React from "react";
 
+import Chart from "@/components/Chart";
+
+import SVG_calendar from "@/public/svgs/calendar.svg";
+import SVG_share from "@/public/svgs/share.svg";
+import SVG_chart from "@/public/svgs/barchart.svg";
+import Button from "@/components/Button";
+
 const ScheduleSummary = () => {
-  return <div className="box-schedule-side-layout flex-col-center">다</div>;
+  return (
+    <div className="box-schedule-side-layout flex-col-center">
+      <div className="w-[340px] mt-[30px]">
+        <p className="text-left font-bold">화/13 12:00 희망자</p>
+        <div className="card-layout centered-content h-[160px] p-[20px]">
+          사자,강아지,호랑이,오리,코끼리
+        </div>
+      </div>
+      <Button
+        className="card-layout centered-content w-[250px] h-[60px] mt-[5px] mb-[25px] text-[20px]"
+        onClick={() => {
+          console.log("일정등록 버튼");
+        }}
+      >
+        <SVG_calendar />
+        <span className="ml-[4px]">일정 등록</span>
+      </Button>
+      <Chart />
+      <div className="flex">
+        <Button
+          className="card-layout w-[120px] h-[50px] mr-[10px] centered-content"
+          onClick={() => {
+            console.log("공유버튼클릭");
+          }}
+        >
+          <SVG_share />
+          <span>링크 공유</span>
+        </Button>
+        <Button
+          className="card-layout w-[120px] h-[50px] centered-content"
+          onClick={() => {
+            console.log("공유버튼클릭");
+          }}
+        >
+          <SVG_chart />
+          <span>그래프 공유</span>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ScheduleSummary;
