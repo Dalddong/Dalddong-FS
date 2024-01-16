77fd4cc69d43308fa17dcd8210d062c189b134a9
youngduck <dudejr5839@gmail.com>
2024-01-16 11:09:01 +0900

    fix: DD-45 next auth 주석삭제

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -15,12 +15,7 @@ const handler = NextAuth({
       clientSecret: process.env.NAVER_CLIENT_SECRET!,
     }),
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: {
           label: "이메일",
