64f467bee49395d8fa3e561f075a3c7205a1c499
youngduck <dudejr5839@gmail.com>
2024-01-29 13:34:23 +0900

    feat: api-doc swagger추가

--- app/api/schedule/[id]/route.ts
+++ app/api/schedule/[id]/route.ts
@@ -1,3 +1,149 @@
+/**
+ * @swagger
+ * /api/schedule/{id}:
+ *   get:
+ *     summary: 일정 조회
+ *     description: Retrieve schedule details by providing its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the schedule
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Schedule details retrieved successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: OK
+ *               data:
+ *                 _id:
+ *                   type: string
+ *                 nomineeDays:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                 nomineePlayTime:
+ *                   type: string
+ *                 schedulePlace:
+ *                   type: string
+ *                 scheduleName:
+ *                   type: string
+ *                 selectDays:
+ *                   type: array
+ *                   items:
+ *                     day:
+ *                       type: string
+ *                     times:
+ *                       type: array
+ *                       items:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *       404:
+ *         description: Schedule not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Schedule not found
+ *   patch:
+ *     summary: 일정 등록
+ *     description: Update a schedule's selectDays by providing its ID and the updated data.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the schedule
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 day:
+ *                   type: string
+ *                   example: "01/29"
+ *                 times:
+ *                   type: array
+ *                   items:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ *                   example:
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *                     - []
+ *
+ *     responses:
+ *       201:
+ *         description: Schedule updated successfully
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Update Schedule success
+ *               data:
+ *                 _id:
+ *                   type: string
+ *                 nomineeDays:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                 nomineePlayTime:
+ *                   type: string
+ *                 schedulePlace:
+ *                   type: string
+ *                 scheduleName:
+ *                   type: string
+ *                 selectDays:
+ *                   type: array
+ *                   items:
+ *                     day:
+ *                       type: string
+ *                     times:
+ *                       type: array
+ *                       items:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *       404:
+ *         description: Schedule not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Schedule not found
+ *       500:
+ *         description: Error updating Schedule
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: Error updating Schedule
+ */
+
 import MongoDBConnect from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 import Schedule from "@/models/schedule";
