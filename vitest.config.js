c8d3f571cec07463d4b776db2864954041d92aa6
youngduck <dudejr5839@gmail.com>
2024-02-19 15:05:40 +0900

    feat: schedule rank test

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import magicalSvgPlugin from 'vite-plugin-magical-svg'
  
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["vitestSetup.ts"],
   },
   resolve:{
     alias:[{
